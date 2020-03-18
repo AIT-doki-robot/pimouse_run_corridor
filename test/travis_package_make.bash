cfb7c16ca80c2a984a765b7f1f3e4293ed9f82fa
Kae Doki <doki@aitech.ac.jp>
2020-03-18 12:44:18 +0900

    Add wall_stop

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -6,6 +6,6 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #bring pimouse_ros to workspace by git clone
 cd ~/catkin_ws/src
-git clone -- depth=1 https://github.com/AIT-doki-robot/pimouse_ros.git
+git clone --depth=1 https://github.com/AIT-doki-robot/pimouse_ros.git
 cd ~/catkin_ws
 catkin_make
