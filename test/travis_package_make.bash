2d269621cbc24c4af1d68b61f781c19992547c73
Kae Doki <doki@aitech.ac.jp>
2020-03-18 09:53:09 +0900

    Add launch and test files

---
+++ test/travis_package_make.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -xve
+
+#sync and make
+#Copy repositories to workspace
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#bring pimouse_ros to workspace by git clone
+cd ~/catkin_ws/src
+git clone -- depth=1 https://github.com/AIT-doki-robot/pimouse_ros.git
+cd ~/catkin_ws
+catkin_make
